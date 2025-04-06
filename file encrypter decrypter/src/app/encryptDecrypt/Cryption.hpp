9bc5405bffe742331946b30df9c8c5e10bce2759
ANMOL YADAV <141206558+Annmool@users.noreply.github.com>
2025-04-06 11:23:07 +0530

    Update Cryption.hpp

--- file encrypter decrypter/src/app/encryptDecrypt/Cryption.hpp
+++ file encrypter decrypter/src/app/encryptDecrypt/Cryption.hpp
@@ -2,7 +2,9 @@
 #define CRYPTION_HPP
 
 #include <string>
+#include <fstream> 
 
-int executeCryption(const std::string& taskData);
 
-#endif
+int executeCryption(const std::string& taskData, int key);
+
+#endif 
