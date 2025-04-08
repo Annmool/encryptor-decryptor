803970bf97e0a16d5296d2a88e10c6523afe760e
ANMOL YADAV <141206558+Annmool@users.noreply.github.com>
2025-04-08 19:22:06 +0530

    Update IO.cpp

--- file encrypter decrypter/src/app/fileHandling/IO.cpp
+++ file encrypter decrypter/src/app/fileHandling/IO.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
 #include "IO.hpp"
 #include <fstream>
-using namespace std;
-IO::IO(const std::string &file_path){
-    file_stream.open(file_path,std::ios::in|std::ios::out|std::ios::binary);
-    if(!file_stream.is_open()){
-        std::cout<<"unable to open the file:"<<file_path<<std::endl;
+#include <string>
+#include <utility>
+#include <stdexcept>
+
+IO::IO(const std::string& file_path) : path_name(file_path) {
+    file_stream.open(file_path, std::ios::in | std::ios::out | std::ios::binary);
+    if (!file_stream.is_open()) {
+        throw std::runtime_error("IO Error: Unable to open file: " + file_path);
     }
 }
 
-
-fstream IO::getFileStream(){
+std::fstream IO::getFileStream() {
     return std::move(file_stream);
 }
 
-IO::~IO(){
-    if(file_stream.is_open()){
+IO::~IO() {
+    if (file_stream.is_open()) {
         file_stream.close();
     }
 }
